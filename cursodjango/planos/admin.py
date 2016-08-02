980ed257c3ea33ec0ad2cb87a4184dd60226655b
Caiua Franca <caiuagomes@gmail.com>
2016-08-02 08:56:08 -0300

    atualização do projeto

--- cursodjango/planos/admin.py
+++ cursodjango/planos/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Plano
 # Register your models here.
 
-admin.site.register(Plano)
+class PlanoAdmin(admin.ModelAdmin):
+
+	list_display = ['name' , 'description', 'price']
+	search_fields = ['name', 'price']
+	prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Plano, PlanoAdmin)
--- cursodjango/planos/models.py
+++ cursodjango/planos/models.py
@@ -10,5 +10,12 @@ class Plano(models.Model):
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
+
 def __str__(self):
 	return self.name
+
+
+class Meta():
+	verbose_name='Plano'
+	verbose_name_plural = 'Planos'
+	orderring = ['name']
